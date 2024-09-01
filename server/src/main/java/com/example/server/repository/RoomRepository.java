501c40b713f40ab6e078d03f7e4f9ed9542549f6
Raunak Bag <raunakbag97@gmail.com>
2024-09-01 21:27:06 +0530

    Add Room Backend Functionality

---
+++ server/src/main/java/com/example/server/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.example.server.repository;
+
+import com.example.server.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+
+
+}
---
+++ server/src/main/java/com/example/server/service/IRoomService.java
@@ -0,0 +1,12 @@
+package com.example.server.service;
+
+import com.example.server.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+public interface IRoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws SQLException, IOException;
+}
